2afcab11fc4dd55bb50bf7795c60a1af110c1231
MarcoFalke <falke.marco@gmail.com>
2018-09-24 16:10:13 -0400

    Merge #14007: tests: Run functional test on Windows and enable it on Appveyor
    661ac15a4a appveyor: Run functional tests on appveyor (Chun Kuan Lee)
    2148c36b6e tests: Make it possible to run functional tests on Windows (Chun Kuan Lee)
    
    Pull request description:
    
      This PR do the following things:
      - Make functional tests compatible with Windows
      - Print color output in functional tests for Windows 10
      - Run util and functional tests on appveyor
      - Do not run symlink tests on Windows
    
      Note:
      - The wallet_multiwallet.py fail is unrelated to the test framework, it's a bug related to c++ code or maybe dependencies. `bitcoind` would exit with 0xC0000005(Access violation) during shutdown occasionally. Disable this for now.
      - Not using `--failfast` because this is still in experimental. We should track if there is any other error.
      - Disable ZMQ tests because the python zmq library could cause access violation sometimes.
      - Disable `feature_notifications` because Bitcoin Core handles the command in different thread, whicha can cause a race condition.
    
    Tree-SHA512: b76db137d264e62a5c130e1cbca7a2ca002a7a0f4153fa0b92c1ea6c9c09ef0533e11c49bdbd566c472d8ff59f245758feb5e5a6ec6cb6bb66a1c67bab5fa48a

--- test/functional/feature_block.py
+++ test/functional/feature_block.py
@@ -827,8 +827,8 @@ class FullBlockTest(BitcoinTestFramework):
         tx.vout.append(CTxOut(0, script_output))
         tx.vin.append(CTxIn(COutPoint(b64a.vtx[1].sha256, 0)))
         b64a = self.update_block("64a", [tx])
-        assert_equal(len(b64a.serialize()), MAX_BLOCK_SIZE + 8)
-        self.send_blocks([b64a], success=False, reject_code=1, reject_reason=b'error parsing message')
+        assert_equal(len(b64a.serialize()), MAX_BLOCK_BASE_SIZE + 8)
+        self.sync_blocks([b64a], success=False, reject_code=1, reject_reason='non-canonical ReadCompactSize():')
 
         # dashd doesn't disconnect us for sending a bloated block, but if we subsequently
         # resend the header message, it won't send us the getdata message again. Just
